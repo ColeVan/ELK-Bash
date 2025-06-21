bb8419c2341b03c49a0c016e5b5a4cbb0348e891
ColeVan <70167373+ColeVan@users.noreply.github.com>
2025-06-21 15:44:33 -0400

    Update orchestrate.sh

--- scripts/orchestrate.sh
+++ scripts/orchestrate.sh
@@ -4,6 +4,6 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 source "$SCRIPT_DIR/functions.sh"
-source "$SCRIPT_DIR/p00.sh"
-source "$SCRIPT_DIR/p01.sh"
-source "$SCRIPT_DIR/p02.sh"
+source "$SCRIPT_DIR/foundation.sh"
+source "$SCRIPT_DIR/service_install_setup.sh"
+source "$SCRIPT_DIR/agent_install_fleet_setup.sh"
